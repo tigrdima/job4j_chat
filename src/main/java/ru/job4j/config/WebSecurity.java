0fe8ee7b727bbf8049e0c6d44a66dea02e376ee6
tigrdima <duys79yandex.ru>
2022-07-20 15:21:00 +0300

    3. Авторизация JWT [#9146 #324124]

--- src/main/java/ru/job4j/config/WebSecurity.java
+++ src/main/java/ru/job4j/config/WebSecurity.java
@@ -53,4 +53,5 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
     }
+
 }
